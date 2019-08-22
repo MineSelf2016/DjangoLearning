81cec263ac143597335378931b8b37be5ff0e38d
MineSelf2016 <1160131684@qq.com>
2019-08-22 11:02:24 +0800

    完成部分页面设计

--- ZhizheDemo/README.md
+++ ZhizheDemo/README.md
@@ -2,9 +2,16 @@
 作者
 <ul>
     <li>Qi Cong
-    <li>Zhe Ying Huang
+    <li>Huang Zheying
 </ul>
 
 ## 项目搭建
-应用：rumour
+应用：rumour，包含首页index.html 与结果页result.html
+
+### index.html
+用于用户输入谣言文本，提交至Django 进行结果预测。
+页面主体即为一个form 表单
+
+### result.html
+用于获得预测的谣言结果，结果值为(0, 1)，越接近1，则是谣言的可能性越大。
 
--- ZhizheDemo/rumour/views.py
+++ ZhizheDemo/rumour/views.py
@@ -10,7 +10,38 @@ def index(request):
 
 def result(request):
 
-    context = dict()
-    return render(request, "rumour/result.html", context)
+    rumour_content = None
+    rumour_prob = rumour_predict_label = rumour_true_label = None
+    rumour_source = rumour_length = None
+    rumour_publish_date = rumour_submit_date = None
+    context = None
+
+    try:
+        rumour_content = request.POST["rumourContent"]
+        rumour_source = request.POST["rumourSource"]
+        rumour_length = request.POST["rumourLength"]
+        rumour_publish_date = request.POST["rumourPublishDate"]
+    except Exception as e:
+        context = {
+            "error_message" : "Please input necessary field"
+        }
+        return render(request, "rumour/index.html", context)
+    
+
+    if len(rumour_content) == 0:
+        # 重新显示index.html 页面
+        context = {
+            "error_message" : "You didn't input rumour text."
+        }
+        return render(request, "rumour/index.html", context)
+    else:
+        rumour_prob = 0.6
+        rumour_prob_str = "有较大可能性是谣言"
+        context = {
+            "rumour_content" : rumour_content,
+            "rumour_prob" : rumour_prob,
+            "rumour_prob_str" : rumour_prob_str
+        }
+        return render(request, "rumour/result.html", context)
 
     
